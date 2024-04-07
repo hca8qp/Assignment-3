cd6fdac7992f44fa8c6d054854befce9789e66ba
hca8qp <143734817+hca8qp@users.noreply.github.com>
2024-04-07 18:34:17 -0500

    Create LinearSearch.h

---
+++ LinearSearch.h
@@ -0,0 +1,10 @@
+#ifndef LINEAR_SEARCH_H
+#define LINEAR_SEARCH_H
+
+#include <vector>
+
+// Declaration of the last_occurrence function template
+template <typename Item_Type>
+int last_occurrence(const std::vector<Item_Type>& items, const Item_Type& target);
+
+#endif // LINEAR_SEARCH_H
